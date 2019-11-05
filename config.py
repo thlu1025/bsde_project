f5c56a0eb6891ac6b7e39d92695f4ded1bcde6e9
yiwei-shi <55965508+yiwei-shi@users.noreply.github.com>
2019-11-05 10:36:43 -0500

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class HJBConfig(Config):
     # Y_0 is about 4.5901.
     dim = 100
     total_time = 1.0
-    num_time_interval = 20
+    num_time_interval = 10
     lr_boundaries = [400]
     num_iterations = 2000
     lr_values = list(np.array([1e-2, 1e-2]))
